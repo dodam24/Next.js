0f2f85e261dbb1bc1b7a9bb156680fda02ca8dd5
dodam24 <dodam0724@gmail.com>
2024-01-18 16:22:36 +0900

    환경변수 설정

--- nextapp/src/app/read/[id]/page.js
+++ nextapp/src/app/read/[id]/page.js
@@ -1,5 +1,5 @@
 export default async function Read(props) {
-  const res = await fetch(`http://localhost:9999/topics/${props.params.id}`, {  // id값을 URL에 반영해서 데이터를 가져온다.
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL+`topics/${props.params.id}`, {  // id값을 URL에 반영해서 데이터를 가져온다.
     cache: "no-store",
   });
   const topic = await res.json();
